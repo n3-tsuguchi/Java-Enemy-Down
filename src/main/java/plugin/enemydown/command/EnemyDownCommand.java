3144e36eaa150ca128c2b31ca50d9a64a68d650f
tsuguchi0 <tsuguchi@nnn.or.jp>
2023-09-10 13:17:42 +0900

    Java中級プラグイン開発スタート
    EnemyDownCommand.javaファイルに敵がランダムで出現させるコマンドを追加。
    
    ゲームスタート時の装備をネザライトに変更するソースコードを追加

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -3,6 +3,7 @@ package plugin.enemydown.command;
 import java.util.List;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,6 +11,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class EnemyDownCommand implements CommandExecutor {
 
@@ -18,14 +21,31 @@ public class EnemyDownCommand implements CommandExecutor {
     if (sender instanceof Player player) {
       World world = player.getWorld();
 
-      player.setHealth(20);
-      player.setFoodLevel(20);
+      // プレイヤーの状態を初期化する。（体力と空腹度を最大値にする）
+      initPlayerStatus(player);
 
       world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
     return false;
   }
 
+  /**
+   * ゲームを始める前にプレイヤーの状態を設定する。
+   * 体力と空腹度を最大にして、装備はネザライト装備にする。
+   *
+   * @param player　コマンドを実行したプレイヤー
+   */
+  private void initPlayerStatus(Player player) {
+    player.setHealth(20);
+    player.setFoodLevel(20);
+
+    PlayerInventory inventory = player.getInventory();
+    inventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+    inventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
+    inventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
+    inventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
+    inventory.setItemInMainHand(new ItemStack(Material.NETHERITE_SWORD));
+  }
 
 
   /**
