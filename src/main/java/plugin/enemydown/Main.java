08112cedebb3052374ded583a65590b179c7ca8c
n3-tsuguchi <tsuguchi@nnn.or.jp>
2023-09-19 20:14:11 +0900

    Merge remote-tracking branch 'origin/master'
    データベース接続用のソースコードを追加しました。

--- src/main/java/plugin/enemydown/Main.java
+++ src/main/java/plugin/enemydown/Main.java
@@ -1,6 +1,8 @@
 package plugin.enemydown;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemydown.command.EnemyDownCommand;
 
@@ -8,7 +10,12 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        EnemyDownCommand enemyDownCommand = new EnemyDownCommand(this);
+        EnemyDownCommand enemyDownCommand = new EnemyDownCommand(this) {
+            @Override
+            public boolean onExecuteNPCCommand(CommandSender sender, Command command, String label, String[] args) {
+                return false;
+            }
+        };
         Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);
         getCommand("enemyDown").setExecutor(enemyDownCommand);
         // Plugin startup logic
--- src/main/java/plugin/enemydown/command/BaseCommand.java
+++ src/main/java/plugin/enemydown/command/BaseCommand.java
@@ -13,19 +13,22 @@ public abstract class BaseCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player player) {
-      return onExecutePlayerCommand(player);
+      return onExecutePlayerCommand(player, command, label, args);
     } else {
-      return onExecuteNPCCommand(sender);
+      return onExecuteNPCCommand(sender, command, label, args);
     }
   }
 
   /**
    * コマンド実行者がプレイヤーだった場合に実行します。
    * @param player　コマンドを実行したプレイヤー
+   * @param command　コマンド
+   * @param label　コマンドのラベル
+   * @param args　コマンドの引数
    * @return　処理の実行有無
    */
 
-  public abstract boolean onExecutePlayerCommand(Player player);
+  public abstract boolean onExecutePlayerCommand(Player player, Command command, String label, String[] args);
 
   /**
    * コマンド実行者がプレイヤー以外だった場合に実行します。
@@ -33,5 +36,11 @@ public abstract class BaseCommand implements CommandExecutor {
    * @return　処理の実行有無
    */
 
+  public abstract boolean onExecuteNPCCommand(CommandSender sender, Command command, String label, String[] args);
+
   public abstract boolean onExecuteNPCCommand(CommandSender sender);
+
+  public abstract boolean onExecutePlayerCommand(Player player);
+
+  public abstract boolean onExecuteNPCCommand(CommandSender sender, String label, String[] args);
 }
