f6e9a20d05f8a119aab7b0afe48d387518121071
n3-tsuguchi <tsuguchi@nnn.or.jp>
2023-09-21 17:49:29 +0900

    Merge remote-tracking branch 'origin/master'
    データベース接続用のソースコードを追加しました。
    
    データベース接続用のソースコードを正しく変更しました。
    
    JDBCを使ってスコア情報の登録のソースコードを追加しました。
    
    JDBCをMyBatisにソースコードを変更しました。

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -2,9 +2,9 @@ package plugin.enemydown.command;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
@@ -13,6 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.SplittableRandom;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -30,7 +34,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.potion.PotionEffect;
 import plugin.enemydown.Main;
-import plugin.enemydown.data.PlayerScore;
+import plugin.enemydown.data.ExecutingPlayer;
+import plugin.enemydown.mapper.data.PlayerScoreMapper;
+import plugin.enemydown.mapper.data.data.PlayerScore;
 
 
 /**
@@ -50,37 +56,39 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
 
   private Main main;
 
-  private List<PlayerScore> playerScoreList = new ArrayList<>();
+  private List<ExecutingPlayer> executingPlayerList = new ArrayList<>();
   private List<Entity> spwanEntityList = new ArrayList<>();
+
+  private SqlSessionFactory sqlSessionFactory;
 
   public EnemyDownCommand(Main main) {
     this.main = main;
+
+    try {
+      InputStream  inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+      this.sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
   public boolean onExecutePlayerCommand(Player player, Command command, String label, String[] args) {
     if (args.length == 1 && LIST.equals(args[0])) {
+      try (SqlSession session = sqlSessionFactory.openSession()) {
+        PlayerScoreMapper mapper = session.getMapper(PlayerScoreMapper.class);
 
-      try (Connection con = DriverManager.getConnection(
-          "jdbc:mysql://localhost:3306/spigot_server",
-          "root",
-          "zgaudam91");
-          Statement statement = con.createStatement();
-          ResultSet resultset = statement.executeQuery("select * from player_score;")) {
-        while (resultset.next()) {
-          int id = resultset.getInt("id");
-          String name = resultset.getString("player_name");
-          int score = resultset.getInt("score");
-          String difficulty = resultset.getString("difficulty");
+        DateTimeFormatter formatter = ofPattern("yyyy-MM-dd HH:mm:ss");
+        List<PlayerScore> playerScoreList = mapper.selectList();
+        for (PlayerScore playerScore : playerScoreList) {
+          LocalDateTime date = LocalDateTime.parse(playerScore.getRegisteredAt(), formatter);
 
-          DateTimeFormatter formatter = ofPattern("yyyy-MM-dd HH:mm:ss");
-          LocalDateTime date = LocalDateTime.parse(resultset.getString("registered_at"), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-
-          player.sendMessage(id + " " + name + " " + score + " " + difficulty + " " + date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+          player.sendMessage(playerScore.getId() + " "
+              + playerScore.getPlayerName() + " "
+              + playerScore.getScore() + " "
+              + playerScore.getDifficulty() + " "
+              + date.format(formatter));
         }
-      } catch (SQLException e) {
-        e.printStackTrace();
       }
       return false;
     }
@@ -89,13 +97,13 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
       return false;
     }
 
-    PlayerScore nowPlayerScore = getPlayerScore(player);
+    ExecutingPlayer nowExecutingPlayer = getPlayerScore(player);
 
 
     // プレイヤーの状態を初期化する。（体力と空腹度を最大値にする）
     initPlayerStatus(player);
 
-    gamePlay(player, nowPlayerScore, difficulty);
+    gamePlay(player, nowExecutingPlayer, difficulty);
     return true;
   }
 
@@ -135,7 +143,7 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
       return;
     }
 
-    playerScoreList.stream()
+    executingPlayerList.stream()
         .filter(p -> p.getPlayerName().equals(player.getName()))
         .findFirst()
         .ifPresent(p -> {
@@ -157,23 +165,23 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
    * @return 現在実行しているプレイヤーのスコア情報
    */
 
-  private PlayerScore getPlayerScore(Player player) {
-    PlayerScore playerScore = new PlayerScore(player.getName());
-    if (playerScoreList.isEmpty()) {
-      playerScore = addNewPlayer(player);
+  private ExecutingPlayer getPlayerScore(Player player) {
+    ExecutingPlayer executingPlayer = new ExecutingPlayer(player.getName());
+    if (executingPlayerList.isEmpty()) {
+      executingPlayer = addNewPlayer(player);
     } else {
-      playerScore = playerScoreList.stream()
+      executingPlayer = executingPlayerList.stream()
           .findFirst()
           .map(ps -> ps.getPlayerName().equals(player.getName())
               ? ps
-              : addNewPlayer(player)).orElse(playerScore);
+              : addNewPlayer(player)).orElse(executingPlayer);
     }
 
-    playerScore.setGameTime(GAME_TIME);
-    playerScore.setScore(0);
+    executingPlayer.setGameTime(GAME_TIME);
+    executingPlayer.setScore(0);
     player.getActivePotionEffects();
     removePotionEffect(player);
-    return playerScore;
+    return executingPlayer;
   }
 
 
@@ -185,9 +193,9 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
    * @return 新規のプレイヤー
    */
 
-  private PlayerScore addNewPlayer(Player player) {
-    PlayerScore newPlayer= new PlayerScore(player.getName());
-    playerScoreList.add(newPlayer);
+  private ExecutingPlayer addNewPlayer(Player player) {
+    ExecutingPlayer newPlayer= new ExecutingPlayer(player.getName());
+    executingPlayerList.add(newPlayer);
     return newPlayer;
   }
 
@@ -214,17 +222,17 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
    * ゲームを開始します。規定の時間内に敵を倒すとスコアが加算されます。合計スコアを時間経過後に表示します。
    *
    * @param player　コマンドを実行したプレイヤー
-   * @param nowPlayerScore　プレイヤースコア情報
+   * @param nowExecutingPlayer　プレイヤースコア情報
    * @param difficulty　難易度
    */
 
-  private void gamePlay(Player player, PlayerScore nowPlayerScore, String difficulty) {
+  private void gamePlay(Player player, ExecutingPlayer nowExecutingPlayer, String difficulty) {
     Bukkit.getScheduler().runTaskTimer(main, Runnable -> {
-      if (nowPlayerScore.getGameTime() <= 0) {
+      if (nowExecutingPlayer.getGameTime() <= 0) {
         Runnable.cancel();
 
         player.sendTitle("ゲームが終了しました。",
-            nowPlayerScore.getPlayerName() + "合計" + nowPlayerScore.getScore() + "点！",
+            nowExecutingPlayer.getPlayerName() + "合計" + nowExecutingPlayer.getScore() + "点！",
             0, 60, 0);
 
         spwanEntityList.forEach(Entity::remove);
@@ -237,7 +245,7 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
             Statement statement = con.createStatement()) {
           statement.executeUpdate(
               "insert into player_score (player_name, score, difficulty, registered_at) "
-                  + "values ('" + nowPlayerScore.getPlayerName() + "', " + nowPlayerScore.getScore() + ", '"
+                  + "values ('" + nowExecutingPlayer.getPlayerName() + "', " + nowExecutingPlayer.getScore() + ", '"
                   + difficulty + "', '" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "');");
 
         } catch (SQLException e) {
@@ -249,7 +257,7 @@ public abstract class  EnemyDownCommand extends BaseCommand implements Listener 
       }
       Entity spawnEntity = player.getWorld().spawnEntity(getEnemySpawnLocation(player), getEnemy(difficulty));
       spwanEntityList.add(spawnEntity);
-      nowPlayerScore.setGameTime(nowPlayerScore.getGameTime() - 5);
+      nowExecutingPlayer.setGameTime(nowExecutingPlayer.getGameTime() - 5);
     }, 0, 5 * 20);
   }
   /**
